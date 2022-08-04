ffca61a252566cdad06437412afb13b15add9cb7
Matt Dittes <mattdittes@gmail.com>
2022-08-03 21:00:08 -0400

    Titles appear

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const url = 'https://api.tvmaze.com/shows/'
 
-const card1 = document.querySelector('.card-body1')
-const card2 = document.querySelector('.card-body2')
+const cardName1 = document.querySelector('#card-title1')
+const cardName2 = document.querySelector('#card-title2')
+const cardImage1 = document.querySelector('#card-image1')
+const cardImage2 = document.querySelector('#card-image2')
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(0);
-    max = Math.floor(240);
+    max = Math.floor(5000);
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 let id1 = getRandomIntInclusive()
@@ -13,8 +15,22 @@ let id2 = getRandomIntInclusive()
 
 fetch(`${url}${id1}`)
 .then(res => res.json())
-.then(data => console.log(data))
+.then(renderOne)
 
 fetch(`${url}${id2}`)
 .then(res => res.json())
-.then(data => console.log(data))
+.then(renderTwo)
+
+function renderOne(data) {
+    const title = data.name 
+    cardName1.append(title)
+
+    image.src = data.image.medium
+    cardImage1.append(image.src)
+    console.log(data)
+}
+
+function renderTwo(data) {
+    const title = data.name
+    cardName2.append(title)
+}
